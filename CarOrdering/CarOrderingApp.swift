ccfd6f737f3c352af616e07f66d8488762cda00f
Akinbande Gbenga <olugbengalium@gmail.com>
2023-05-26 09:19:10 +0100

    Initial Commit

---
+++ CarOrdering/CarOrderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarOrderingApp.swift
+//  CarOrdering
+//
+//  Created by Gbenga Abayomi on 26/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CarOrderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
